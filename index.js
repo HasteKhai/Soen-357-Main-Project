f3b2b47b8c1a0921a71f7ab92d15ced2dc3a8631
Huy minh le <minhking22@gmail.com>
2024-04-13 14:31:27 -0400

    updated

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => { 
 	nav.classList.toggle("navclose"); 
 })
+
+function logout() {
+	// Perform logout action here, such as redirecting to a logout page or triggering a logout API request
+	window.location.href = "login.html"; // Example redirect to a logout PHP script
+}
